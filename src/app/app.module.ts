d9a75d2995272fe665feb314cddc6751f604faeb
Josue Cardona <josuecardonap@gmail.com>
2018-01-09 21:24:01 -0500

    Styling Installations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
+//Third-Party Modules
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+//Feature Modules
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [ AppComponent ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpModule,
+    ReactiveFormsModule,
+    NgbModule.forRoot()
+    // AppRoutingModule,
+    // InMemoryWebApiModule.forRoot(),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
